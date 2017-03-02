9b4799408e50a01447d3043220a1d9a7a7c0e509
rakshitbharat <rakshitbharat@gmail.com>
2017-03-02 23:21:04 +0530

    securities ready

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -9,7 +9,7 @@
             <ul class="nav navbar-nav">
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                        <span class="hidden-xs">Alexander Pierce</span>
+                        <span class="hidden-xs">{{ Auth::user()->name }}</span>
                     </a>
                     <ul class="dropdown-menu">
                         <li class="user-body">
