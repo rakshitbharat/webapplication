503e4b3d8c28ad8389783ef3b50347a2570b792f
kevinpatel110212 <kevinpatel57@gmail.com>
2017-03-04 19:42:08 +0530

    edit profile changed layout

--- app/Models/AccountType.php
+++ app/Models/AccountType.php
@@ -1,16 +1,17 @@
-<?php namespace App\Models;
+<?php
+
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Validator;
 
 class AccountType extends Model {
 
     /**
      * Generated
      */
-
     protected $table = 'accountType';
     protected $fillable = ['id', 'name', 'userId'];
-
 
     public function user() {
         return $this->belongsTo(\App\Models\User::class, 'userId', 'id');
@@ -19,25 +20,39 @@ class AccountType extends Model {
     public function accounts() {
         return $this->hasMany(\App\Models\Account::class, 'accountTypeId', 'id');
     }
+
     public static function dataOperation($request) {
-        if($request->method() == 'GET'){
-            if($request->id){
+        if ($request->method() == 'GET') {
+            if ($request->id) {
                 return AccountType::find($request->id);
-            }else{
+            } else {
                 return AccountType::all();
             }
         }
-        if($request->method() == 'POST'){
+        if ($request->method() == 'POST') {
             AccountType::validator($request->all())->validate();
-            if($request->id){
+            if ($request->id) {
                 $AccountType = AccountType::find($request->id);
                 return $AccountType->update($request->all());
-            }else{
+            } else {
                 $AccountType = new AccountType();
                 return $AccountType->create($request->all());
             }
         }
     }
 
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array $request
+     *
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected static function validator($request) {
+        return Validator::make(
+                        $request, [
+                    'name' => 'required|max:255',
+        ]);
+    }
 
 }
