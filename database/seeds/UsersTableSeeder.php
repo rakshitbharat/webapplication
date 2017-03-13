7d356068d59a0572eee571a0b4b10404b0b91dfc
Afaz <Afaz@DESKTOP-582OR9E>
2017-03-13 20:40:53 +0530

    added database seeder.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,24 +1,26 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
-class UsersTableSeeder extends Seeder
-{
+class UsersTableSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
+        //$carbon = new \Carbon\Carbon::now()->toDateTimeString();
         DB::table('users')->insert([
-            'name' => 'timestart',
-            'email' => 'test@gmail.com',
-            'role' => 'admin',
-            'confirmed' => 1,
-            'created_at' => time(),
-            'updated_at' => time(),
-            'password' => bcrypt('12345678'),
+        'name' => 'timestart',
+        'email' => 'test@gmail.com',
+        'role' => 'admin',
+        'confirmed' => 1,
+        'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+        'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+        'password' => bcrypt('12345678'),
         ]);
     }
+
 }
