24c6db9f0f2796eb3dd7259a01084dee44207c33
rakshitbharat <rakshitbharat@gmail.com>
2017-03-14 01:00:41 +0530

    added setting bar

--- resources/views/admin/jsLink.blade.php
+++ resources/views/admin/jsLink.blade.php
@@ -1,6 +1,7 @@
 <script src="{{ asset('public/adminAsset/bootstrap/js/bootstrap.min.js') }}" ></script>
 <script src="{{ asset('public/adminAsset/plugins/fastclick/fastclick.min.js') }}" ></script>
 <script src="{{ asset('public/adminAsset/dist/js/app.min.js') }}" ></script>
+<script src="{{ asset('public/adminAsset/dist/js/demo.js') }}" ></script>
 <script src="{{ asset('public/adminAsset/plugins/slimScroll/jquery.slimscroll.min.js') }}" ></script>
 <script src="{{asset('public/jquery-validation/dist/jquery.validate.min.js')}}" type="text/javascript"></script>
 <script src="{{asset('public/jquery-validation/dist/additional-methods.min.js')}}" type="text/javascript"></script>
