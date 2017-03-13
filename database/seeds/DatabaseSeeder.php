380887f599d35c853c414bf9997beff41f9c089f
Afaz <Afaz@DESKTOP-582OR9E>
2017-03-13 19:38:04 +0530

    added database seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'timestart',
+            'email' => 'test@gmail.com',
+            'role' => 'admin',
+            'confirmed' => 1,
+            'created_at' => time(),
+            'updated_at' => time(),
+            'password' => bcrypt('12345678'),
+        ]);
     }
 }
